75faebca81f375e79cd8d27813cbb5931676580f
Chicring <3081044735@qq.com>
2024-07-31 00:32:26 +0800

    fix: 图片加载策略

--- lib/src/features/emby/presentation/emby_home/emby_home_screen.dart
+++ lib/src/features/emby/presentation/emby_home/emby_home_screen.dart
@@ -1,12 +1,10 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:go_router/go_router.dart';
 import 'package:themby/src/common/domiani/site.dart';
-import 'package:themby/src/common/widget/header_text.dart';
 import 'package:themby/src/features/emby/application/emby_state_service.dart';
 import 'package:themby/src/features/emby/data/image_repository.dart';
 import 'package:themby/src/features/emby/data/view_repository.dart';
